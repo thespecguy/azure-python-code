0807d2fd605621bef88a4bcc7a1c5ff030a7d5cf
satyam <satyamcs1999@gmail.com>
2021-07-02 16:56:09 +0530

    third update

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, <b>Satyam!</b> <img src='https://thestoragess.blob.core.windows.net/testcon/PhotoGrid_1590937364961.jpg'/>"
+    return "Hello, <b>Satyam!</b> <img src='https://thestoragess.blob.core.windows.net/testcon/PhotoGrid_1590937364961.jpg' width='100' height='200'/>"
